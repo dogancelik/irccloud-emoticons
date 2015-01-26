3d0ef546e01af52bfa58f86ecf8c23f9f6138a5f
Doğan Çelik <dogancelik93@gmail.com>
2015-01-26 05:30:30 +0200

    Fix layout

--- src/twitch_emoticons.meta.js
+++ src/twitch_emoticons.meta.js
@@ -3,7 +3,7 @@
 // @namespace   dogancelik.com
 // @description Enables Twitch emoticons in IRCCloud
 // @include     https://www.irccloud.com/*
-// @version     1.0.2
+// @version     1.0.3
 // @grant       none
 // @updateURL   https://github.com/dogancelik/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.meta.js
 // @downloadURL https://github.com/dogancelik/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.user.js
